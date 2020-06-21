a6a2142269d5c359a11aa9e727b0fc8aa7d53924
zhangdong <wwwzdiwj@sina.com>
2020-06-21 17:51:42 +0800

    打印分支 dev0.0.1

--- src/com/zhangdong/helloword/helloWord.java
+++ src/com/zhangdong/helloword/helloWord.java
@@ -4,5 +4,6 @@ public class helloWord {
 
     public static void main(String[] args) {
         System.out.println("hello word!");
+        System.out.println("this is branch dev 0.0.1");
     }
 }
