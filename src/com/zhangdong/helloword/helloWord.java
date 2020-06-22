d55071a8b72aa28c57f1e3aa4dce01bd0a68b32d
zhangd1989 <57430648+zhangd1989@users.noreply.github.com>
2020-06-22 10:40:19 +0800

    Update helloWord.java
    新增减法方法

--- src/com/zhangdong/helloword/helloWord.java
+++ src/com/zhangdong/helloword/helloWord.java
@@ -6,9 +6,14 @@ public class helloWord {
         System.out.println("hello word!");
         System.out.println("this is branch dev 0.0.1");
         System.out.println(sum(1,2));
+        System.out.println(subtraction(4,2));
     }
 
     public static int sum(int a, int b){
         return a+b;
+    }
+
+    public static int subtraction(int a, int b){
+        return a-b;
     }
 }
