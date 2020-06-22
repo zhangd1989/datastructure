207a477c97f3f08a1cccb18a8f437cf535836ecf
zhangdong <wwwzdiwj@sina.com>
2020-06-22 10:22:10 +0800

    添加加法方法

--- src/com/zhangdong/helloword/helloWord.java
+++ src/com/zhangdong/helloword/helloWord.java
@@ -5,5 +5,10 @@ public class helloWord {
     public static void main(String[] args) {
         System.out.println("hello word!");
         System.out.println("this is branch dev 0.0.1");
+        System.out.println(sum(1,2));
+    }
+
+    public static int sum(int a, int b){
+        return a+b;
     }
 }
