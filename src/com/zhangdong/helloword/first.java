78dfb151b0c711e6cf8df065e728b53f8ad225d9
zhangdong <wwwzdiwj@sina.com>
2020-06-29 19:45:45 +0800

    second commit

--- src/com/zhangdong/helloword/first.java
+++ src/com/zhangdong/helloword/first.java
@@ -1,4 +1,8 @@
 package com.zhangdong.helloword;
 
 public class first {
+    public static void main(String[] args) {
+        System.out.println("hello word!");
+        System.out.println("this is branch dev 0.0.1");
+    }
 }
